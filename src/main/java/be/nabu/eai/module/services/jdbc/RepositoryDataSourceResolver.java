c202772ff0651d69b92c20d275d94228ba29d3e3
Alexander Verbruggen <alex@nabu.be>
2020-07-12 21:25:22 +0200

    centralized resolving logic

--- src/main/java/be/nabu/eai/module/services/jdbc/RepositoryDataSourceResolver.java
+++ src/main/java/be/nabu/eai/module/services/jdbc/RepositoryDataSourceResolver.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import be.nabu.eai.repository.EAIResourceRepository;
-import be.nabu.libs.services.ServiceRuntime;
-import be.nabu.libs.services.ServiceUtils;
 import be.nabu.libs.services.jdbc.api.DataSourceWithDialectProviderArtifact;
 import be.nabu.libs.services.jdbc.api.DynamicDataSourceResolver;
 
+// no longer necessary, we have encapsulated this logic into repositories
 public class RepositoryDataSourceResolver implements DynamicDataSourceResolver {
 
 	public static final String REGEX = System.getProperty("be.nabu.datasource.resolver", "^([^.]+)\\..*");
@@ -16,60 +15,63 @@ public class RepositoryDataSourceResolver implements DynamicDataSourceResolver {
 	
 	@Override
 	public String getDataSourceId(String forId) {
-		ServiceRuntime runtime = ServiceRuntime.getRuntime();
-		if (runtime != null) {
-			List<DataSourceWithDialectProviderArtifact> artifacts = EAIResourceRepository.getInstance().getArtifacts(DataSourceWithDialectProviderArtifact.class);
-
-			// let's check if a pool has been explicitly configured for this artifact
-			String longest = getContextualFor(forId, artifacts);
-			if (longest != null) {
-				return longest;
-			}
-			
-			// we did not find for the explicit artifact, lets try the service context
-			String context = ServiceUtils.getServiceContext(runtime);
-			// let's check if a pool has been explicitly configured for this service context
-			longest = getContextualFor(context, artifacts);
-			if (longest != null) {
-				return longest;
-			}
-			
-			// get related hits for the service context
-			List<DataSourceWithDialectProviderArtifact> hits = getRelatedFor(context, artifacts);
-			
-			if (hits.isEmpty()) {
-				hits = getRelatedFor(forId, artifacts);
-			}
-
-			// if we have exactly one hit, return that
-			if (hits.size() == 1) {
-				return hits.get(0).getId();
-			}
-			// if we have multiple, do a match based on ids, the closest one wins
-			else if (hits.size() > 1) {
-				DataSourceWithDialectProviderArtifact closest = null;
-				int closestMatch = 0;
-				
-				String[] partsToMatch = forId.split("\\.");
-				for (DataSourceWithDialectProviderArtifact hit : hits) {
-					String[] parts = hit.getId().split("\\.");
-					int matchRate = 0;
-					for (int i = 0; i < Math.min(partsToMatch.length, parts.length); i++) {
-						if (partsToMatch[i].equals(parts[i])) {
-							matchRate++;
-						}
-						else {
-							break;
-						}
-					}
-					if (matchRate > closestMatch) {
-						closest = hit;
-					}
-				}
-				return closest == null ? null : closest.getId();
-			}
-		}
-		return null;
+		DataSourceWithDialectProviderArtifact resolveFor = EAIResourceRepository.getInstance().resolveFor(forId, DataSourceWithDialectProviderArtifact.class);
+		return resolveFor == null ? null : resolveFor.getId();
+		
+//		ServiceRuntime runtime = ServiceRuntime.getRuntime();
+//		if (runtime != null) {
+//			List<DataSourceWithDialectProviderArtifact> artifacts = EAIResourceRepository.getInstance().getArtifacts(DataSourceWithDialectProviderArtifact.class);
+//
+//			// let's check if a pool has been explicitly configured for this artifact
+//			String longest = getContextualFor(forId, artifacts);
+//			if (longest != null) {
+//				return longest;
+//			}
+//			
+//			// we did not find for the explicit artifact, lets try the service context
+//			String context = ServiceUtils.getServiceContext(runtime);
+//			// let's check if a pool has been explicitly configured for this service context
+//			longest = getContextualFor(context, artifacts);
+//			if (longest != null) {
+//				return longest;
+//			}
+//			
+//			// get related hits for the service context
+//			List<DataSourceWithDialectProviderArtifact> hits = getRelatedFor(context, artifacts);
+//			
+//			if (hits.isEmpty()) {
+//				hits = getRelatedFor(forId, artifacts);
+//			}
+//
+//			// if we have exactly one hit, return that
+//			if (hits.size() == 1) {
+//				return hits.get(0).getId();
+//			}
+//			// if we have multiple, do a match based on ids, the closest one wins
+//			else if (hits.size() > 1) {
+//				DataSourceWithDialectProviderArtifact closest = null;
+//				int closestMatch = 0;
+//				
+//				String[] partsToMatch = forId.split("\\.");
+//				for (DataSourceWithDialectProviderArtifact hit : hits) {
+//					String[] parts = hit.getId().split("\\.");
+//					int matchRate = 0;
+//					for (int i = 0; i < Math.min(partsToMatch.length, parts.length); i++) {
+//						if (partsToMatch[i].equals(parts[i])) {
+//							matchRate++;
+//						}
+//						else {
+//							break;
+//						}
+//					}
+//					if (matchRate > closestMatch) {
+//						closest = hit;
+//					}
+//				}
+//				return closest == null ? null : closest.getId();
+//			}
+//		}
+//		return null;
 	}
 
 	private String getContextualFor(String forId, List<DataSourceWithDialectProviderArtifact> artifacts) {
